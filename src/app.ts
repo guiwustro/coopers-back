0d87aab7c6833e7550db23643009615f4e588945
Guilherme <gui.wustro@gmail.com>
2023-04-11 17:39:08 -0300

    feat: project data-source and app

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import "express-async-errors";
+import express from "express";
+import cors from "cors";
+
+import { appRoutes } from "./routes";
+import handleErrorMiddleware from "./middlewares/handleError.middleware";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+appRoutes(app);
+
+app.use(handleErrorMiddleware);
+
+export default app;
---
+++ src/data-source.ts
@@ -0,0 +1,37 @@
+import { DataSource } from "typeorm";
+import "dotenv/config";
+
+const AppDataSource = new DataSource(
+	process.env.NODE_ENV === "test"
+		? {
+				type: "sqlite",
+				database: ":memory:",
+				entities: ["src/entities/*.ts"],
+				synchronize: true,
+		  }
+		: {
+				type: "postgres",
+				// url: process.env.DATABASE_URL,
+				ssl:
+					process.env.NODE_ENV === "production"
+						? { rejectUnauthorized: false }
+						: false,
+				logging: true,
+				synchronize: true,
+				entities:
+					process.env.NODE_ENV === "production"
+						? ["dist/src/entities/*.js"]
+						: ["src/entities/*.ts"],
+				migrations:
+					process.env.NODE_ENV === "production"
+						? ["dist/src/migrations/*.js"]
+						: ["src/migrations/*.ts"],
+				host: process.env.DB_HOST,
+				port: 5432,
+				username: process.env.DB_USER,
+				password: process.env.DB_PASSWORD,
+				database: process.env.DB,
+		  }
+);
+
+export default AppDataSource;
